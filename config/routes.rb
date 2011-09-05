cbd890788754a3fefe34921a87d1d06557f3054f
britg <brit@britg.com>
2011-09-05 18:16:25 -0500

    convenience method for devise auth

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ ForgeCraft::Application.routes.draw do
 
   devise_for :users
 
+  devise_scope :user do
+    get "login", :to => "devise/sessions#new"
+    get "register", :to => "devise/registrations#new"
+    get "logout", :to => "devise/sessions#destroy"
+  end
+
   root :to => "users#index"
   resources :users
 
