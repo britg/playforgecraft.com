855aadc7d4adcecc05e0dc59aa0e95e0bf69e43c
britg <brit@britg.com>
2011-09-06 10:04:01 -0500

    try removing sendfile header

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ ForgeCraft::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -24,6 +24,7 @@ ForgeCraft::Application.configure do
   # config.assets.manifest = YOUR_PATH
 
   # Specifies the header that your server uses for sending files
+  config.action_dispatch.x_sendfile_header = nil
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
