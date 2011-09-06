c9e1ceae2b04778030837cb9647b61d51425a4fb
britg <brit@britg.com>
2011-09-05 19:09:43 -0500

    don't allow non-admins to log in

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,14 @@
 
 class AdminController < ApplicationController
 
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :require_admin
+
+  protected
+
+  def require_admin
+    unless current_user.admin?
+      redirect_to logout_path and return false
+    end
+  end
 
 end
