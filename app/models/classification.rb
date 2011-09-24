e1a4d9f26494d8457d2e56c938dacb7de886289d
britg <brit@britg.com>
2011-09-24 12:36:30 -0500

    Define default images for items, classes, and item sets

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -15,6 +15,7 @@ class Classification < ActiveRecord::Base
     :s3_credentials => "#{Rails.root}/config/s3.yml",
     :s3_headers => {'Expires' => 1.year.from_now.httpdate},
     :path => "/:class/:id/:attachment/:style.:extension",
+    :default_url => "http://forgecraft.s3.amazonaws.com/default.jpg",
     :styles => { :thumb => ["100x100#", :jpg], :tiny => ["50x50#", :jpg] }
 
   has_attached_file :default_art,
@@ -22,6 +23,7 @@ class Classification < ActiveRecord::Base
     :s3_credentials => "#{Rails.root}/config/s3.yml",
     :s3_headers => {'Expires' => 1.year.from_now.httpdate},
     :path => "/:class/:id/:attachment/:style.:extension",
+    :default_url => "http://forgecraft.s3.amazonaws.com/default.jpg",
     :styles => { :full => ["640x960#", :jpg], :normal => ["320x480#", :jpg], :small => ["160x240#", :jpg] }
 
   # Class Methods
--- app/models/item.rb
+++ app/models/item.rb
@@ -20,6 +20,7 @@ class Item < ActiveRecord::Base
     :s3_credentials => "#{Rails.root}/config/s3.yml",
     :s3_headers => {'Expires' => 1.year.from_now.httpdate},
     :path => "/:class/:id/:attachment/:style.:extension",
+    :default_url => "http://forgecraft.s3.amazonaws.com/default.jpg",
     :styles => { :thumb => ["100x100#", :jpg], :tiny => ["50x50#", :jpg] }
 
   has_attached_file :art,
@@ -27,6 +28,7 @@ class Item < ActiveRecord::Base
     :s3_credentials => "#{Rails.root}/config/s3.yml",
     :s3_headers => {'Expires' => 1.year.from_now.httpdate},
     :path => "/:class/:id/:attachment/:style.:extension",
+    :default_url => "http://forgecraft.s3.amazonaws.com/default.jpg",
     :styles => { :full => ["640x960#", :jpg], :normal => ["320x480#", :jpg], :small => ["160x240#", :jpg] }
 
   default_scope order("rarity_id asc, name asc")
